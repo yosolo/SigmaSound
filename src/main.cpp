d4a320746c563b74a02db85c8da67913b5e414c1
Yosolo <LeonardButz@outlook.de>
2024-08-12 01:16:15 +0200

    Add imgui

--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,188 @@
 #include "miniaudio.h"
 
 #include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_demo.cpp>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl2.h>
+
+#include "imgui_spectrum.h"
+
+namespace ImGui
+{
+    void StyleColorsSpectrum() {
+        ImGuiStyle* style = &ImGui::GetStyle();
+        style->GrabRounding = 4.0f;
+
+        ImVec4* colors = style->Colors;
+        colors[ImGuiCol_Text] = ColorConvertU32ToFloat4(Spectrum::GRAY800); // text on hovered controls is gray900
+        colors[ImGuiCol_TextDisabled] = ColorConvertU32ToFloat4(Spectrum::GRAY500);
+        colors[ImGuiCol_WindowBg] = ColorConvertU32ToFloat4(Spectrum::GRAY100);
+        colors[ImGuiCol_ChildBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+        colors[ImGuiCol_PopupBg] = ColorConvertU32ToFloat4(Spectrum::GRAY50); // not sure about this. Note: applies to tooltips too.
+        colors[ImGuiCol_Border] = ColorConvertU32ToFloat4(Spectrum::GRAY300);
+        colors[ImGuiCol_BorderShadow] = ColorConvertU32ToFloat4(Spectrum::Static::NONE); // We don't want shadows. Ever.
+        colors[ImGuiCol_FrameBg] = ColorConvertU32ToFloat4(Spectrum::GRAY75); // this isnt right, spectrum does not do this, but it's a good fallback
+        colors[ImGuiCol_FrameBgHovered] = ColorConvertU32ToFloat4(Spectrum::GRAY50);
+        colors[ImGuiCol_FrameBgActive] = ColorConvertU32ToFloat4(Spectrum::GRAY200);
+        colors[ImGuiCol_TitleBg] = ColorConvertU32ToFloat4(Spectrum::GRAY300); // those titlebar values are totally made up, spectrum does not have this.
+        colors[ImGuiCol_TitleBgActive] = ColorConvertU32ToFloat4(Spectrum::GRAY200);
+        colors[ImGuiCol_TitleBgCollapsed] = ColorConvertU32ToFloat4(Spectrum::GRAY400);
+        colors[ImGuiCol_MenuBarBg] = ColorConvertU32ToFloat4(Spectrum::GRAY100);
+        colors[ImGuiCol_ScrollbarBg] = ColorConvertU32ToFloat4(Spectrum::GRAY100); // same as regular background
+        colors[ImGuiCol_ScrollbarGrab] = ColorConvertU32ToFloat4(Spectrum::GRAY400);
+        colors[ImGuiCol_ScrollbarGrabHovered] = ColorConvertU32ToFloat4(Spectrum::GRAY600);
+        colors[ImGuiCol_ScrollbarGrabActive] = ColorConvertU32ToFloat4(Spectrum::GRAY700);
+        colors[ImGuiCol_CheckMark] = ColorConvertU32ToFloat4(Spectrum::BLUE500);
+        colors[ImGuiCol_SliderGrab] = ColorConvertU32ToFloat4(Spectrum::GRAY700);
+        colors[ImGuiCol_SliderGrabActive] = ColorConvertU32ToFloat4(Spectrum::GRAY800);
+        colors[ImGuiCol_Button] = ColorConvertU32ToFloat4(Spectrum::GRAY75); // match default button to Spectrum's 'Action Button'.
+        colors[ImGuiCol_ButtonHovered] = ColorConvertU32ToFloat4(Spectrum::GRAY50);
+        colors[ImGuiCol_ButtonActive] = ColorConvertU32ToFloat4(Spectrum::GRAY200);
+        colors[ImGuiCol_Header] = ColorConvertU32ToFloat4(Spectrum::BLUE400);
+        colors[ImGuiCol_HeaderHovered] = ColorConvertU32ToFloat4(Spectrum::BLUE500);
+        colors[ImGuiCol_HeaderActive] = ColorConvertU32ToFloat4(Spectrum::BLUE600);
+        colors[ImGuiCol_Separator] = ColorConvertU32ToFloat4(Spectrum::GRAY400);
+        colors[ImGuiCol_SeparatorHovered] = ColorConvertU32ToFloat4(Spectrum::GRAY600);
+        colors[ImGuiCol_SeparatorActive] = ColorConvertU32ToFloat4(Spectrum::GRAY700);
+        colors[ImGuiCol_ResizeGrip] = ColorConvertU32ToFloat4(Spectrum::GRAY400);
+        colors[ImGuiCol_ResizeGripHovered] = ColorConvertU32ToFloat4(Spectrum::GRAY600);
+        colors[ImGuiCol_ResizeGripActive] = ColorConvertU32ToFloat4(Spectrum::GRAY700);
+        colors[ImGuiCol_PlotLines] = ColorConvertU32ToFloat4(Spectrum::BLUE400);
+        colors[ImGuiCol_PlotLinesHovered] = ColorConvertU32ToFloat4(Spectrum::BLUE600);
+        colors[ImGuiCol_PlotHistogram] = ColorConvertU32ToFloat4(Spectrum::BLUE400);
+        colors[ImGuiCol_PlotHistogramHovered] = ColorConvertU32ToFloat4(Spectrum::BLUE600);
+        colors[ImGuiCol_TextSelectedBg] = ColorConvertU32ToFloat4((Spectrum::BLUE400 & 0x00FFFFFF) | 0x33000000);
+        colors[ImGuiCol_DragDropTarget] = ImVec4(1.00f, 1.00f, 0.00f, 0.90f);
+        colors[ImGuiCol_NavHighlight] = ColorConvertU32ToFloat4((Spectrum::GRAY900 & 0x00FFFFFF) | 0x0A000000);
+        colors[ImGuiCol_NavWindowingHighlight] = ImVec4(1.00f, 1.00f, 1.00f, 0.70f);
+        colors[ImGuiCol_NavWindowingDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.20f);
+        colors[ImGuiCol_ModalWindowDimBg] = ImVec4(0.20f, 0.20f, 0.20f, 0.35f);
+    }
+}
 
 int main(int argc, char** argv)
 {
     ma_result result;
     ma_engine engine;
 
+    ma_context context;
+    if (ma_context_init(NULL, 0, NULL, &context) != MA_SUCCESS) {
+        return 1;
+    }
+
+    ma_device_info* pPlaybackInfos;
+    ma_uint32 playbackCount;
+    ma_device_info* pCaptureInfos;
+    ma_uint32 captureCount;
+    result = ma_context_get_devices(&context, &pPlaybackInfos, &playbackCount, &pCaptureInfos, &captureCount);
+    if (result != MA_SUCCESS) 
+    {
+        return result;
+    }
+
+    // Loop over each device info and do something with it. Here we just print the name with their index. You may want
+    // to give the user the opportunity to choose which device they'd prefer.
+    for (ma_uint32 iDevice = 0; iDevice < playbackCount; iDevice += 1) 
+    {
+        printf("%d - %s\n", iDevice, pPlaybackInfos[iDevice].name);
+    }
+
+    ma_engine_config engineConfig = ma_engine_config_init();
     result = ma_engine_init(NULL, &engine);
     if (result != MA_SUCCESS) 
     {
         return result;  // Failed to initialize the engine.
     }
 
+#define DELAY_IN_SECONDS    0.2f
+#define DECAY               0.25f   /* Volume falloff for each echo. */
+
+    ma_delay_node delayNode;
+    {
+        ma_delay_node_config delayNodeConfig;
+        ma_uint32 channels;
+        ma_uint32 sampleRate;
+
+        channels = ma_engine_get_channels(&engine);
+        sampleRate = ma_engine_get_sample_rate(&engine);
+
+        delayNodeConfig = ma_delay_node_config_init(channels, sampleRate, (ma_uint32)(sampleRate * DELAY_IN_SECONDS), DECAY);
+
+        result = ma_delay_node_init(ma_engine_get_node_graph(&engine), &delayNodeConfig, NULL, &delayNode);
+        if (result != MA_SUCCESS) {
+            printf("Failed to initialize delay node.");
+            return -1;
+        }
+
+        /* Connect the output of the delay node to the input of the endpoint. */
+        ma_node_attach_output_bus(&delayNode, 0, ma_engine_get_endpoint(&engine), 0);
+    }
+
     ma_sound sound;
     result = ma_sound_init_from_file(&engine, "eple.mp3", 0, nullptr, nullptr, &sound);
+
+    ma_node_attach_output_bus(&sound, 0, &delayNode, 0);
     // ma_sound_set_start_time_in_milliseconds(&sound, 5000.0f);
-    ma_sound_start(&sound);
+    // ma_sound_start(&sound);
 
     glfwInit();
     GLFWwindow* window = glfwCreateWindow(800, 600, "Sound", nullptr, nullptr);
     glfwMakeContextCurrent(window);
 
+
+    ImGui::CreateContext();
+    
+    ImGuiIO& io = ImGui::GetIO();
+    io.Fonts->AddFontFromFileTTF("OpenSans-Regular.ttf", 15.0f);
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable | ImGuiConfigFlags_DockingEnable;
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL2_Init();
+
+    ImGui::StyleColorsSpectrum();
+
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL2_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        
+
+        ImGui::Begin("Test1");
+        ImGui::Text("Hello");
+        if (ImGui::Button("Play")) 
+        {
+            static bool playing = false;
+            if (!playing)
+                ma_sound_start(&sound);
+            else
+                ma_sound_stop(&sound);
+
+            playing = !playing;
+        }
+        ImGui::End();
+
+        ImGui::Begin("Test2");
+        ImGui::End();
+
+        ImGui::ShowDemoWindow();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
+        ImGui::EndFrame();
+        GLFWwindow* backup_current_context = glfwGetCurrentContext();
+        ImGui::UpdatePlatformWindows();
+        ImGui::RenderPlatformWindowsDefault();
+        glfwMakeContextCurrent(backup_current_context);
+
         glfwSwapBuffers(window);
     }
 
