3494f6fa1317cb5568f1e252185945b9b8b8e2eb
Yosolo <LeonardButz@outlook.de>
2024-08-12 02:13:51 +0200

    ImGui stuff and testing

--- src/main.cpp
+++ src/main.cpp
@@ -73,6 +73,7 @@ int main(int argc, char** argv)
     }
 
     ma_device_info* pPlaybackInfos;
+    ma_uint32 playbackIndex;
     ma_uint32 playbackCount;
     ma_device_info* pCaptureInfos;
     ma_uint32 captureCount;
@@ -87,8 +88,15 @@ int main(int argc, char** argv)
     for (ma_uint32 iDevice = 0; iDevice < playbackCount; iDevice += 1) 
     {
         printf("%d - %s\n", iDevice, pPlaybackInfos[iDevice].name);
+        if (pPlaybackInfos[iDevice].isDefault)
+            playbackIndex = iDevice;
     }
 
+    ma_device_config deviceConfig = ma_device_config_init(ma_device_type_playback);
+    deviceConfig.playback.pDeviceID = &pPlaybackInfos[playbackIndex].id;
+
+    ma_device primaryDevice;
+    ma_device_init(&context, &deviceConfig, &primaryDevice);
     ma_engine_config engineConfig = ma_engine_config_init();
     result = ma_engine_init(NULL, &engine);
     if (result != MA_SUCCESS) 
@@ -99,31 +107,31 @@ int main(int argc, char** argv)
 #define DELAY_IN_SECONDS    0.2f
 #define DECAY               0.25f   /* Volume falloff for each echo. */
 
-    ma_delay_node delayNode;
-    {
-        ma_delay_node_config delayNodeConfig;
-        ma_uint32 channels;
-        ma_uint32 sampleRate;
-
-        channels = ma_engine_get_channels(&engine);
-        sampleRate = ma_engine_get_sample_rate(&engine);
-
-        delayNodeConfig = ma_delay_node_config_init(channels, sampleRate, (ma_uint32)(sampleRate * DELAY_IN_SECONDS), DECAY);
-
-        result = ma_delay_node_init(ma_engine_get_node_graph(&engine), &delayNodeConfig, NULL, &delayNode);
-        if (result != MA_SUCCESS) {
-            printf("Failed to initialize delay node.");
-            return -1;
-        }
-
-        /* Connect the output of the delay node to the input of the endpoint. */
-        ma_node_attach_output_bus(&delayNode, 0, ma_engine_get_endpoint(&engine), 0);
-    }
+    // ma_delay_node delayNode;
+    // {
+    //     ma_delay_node_config delayNodeConfig;
+    //     ma_uint32 channels;
+    //     ma_uint32 sampleRate;
+    // 
+    //     channels = ma_engine_get_channels(&engine);
+    //     sampleRate = ma_engine_get_sample_rate(&engine);
+    // 
+    //     delayNodeConfig = ma_delay_node_config_init(channels, sampleRate, (ma_uint32)(sampleRate * DELAY_IN_SECONDS), DECAY);
+    // 
+    //     result = ma_delay_node_init(ma_engine_get_node_graph(&engine), &delayNodeConfig, NULL, &delayNode);
+    //     if (result != MA_SUCCESS) {
+    //         printf("Failed to initialize delay node.");
+    //         return -1;
+    //     }
+    // 
+    //     /* Connect the output of the delay node to the input of the endpoint. */
+    //     ma_node_attach_output_bus(&delayNode, 0, ma_engine_get_endpoint(&engine), 0);
+    // }
 
     ma_sound sound;
     result = ma_sound_init_from_file(&engine, "eple.mp3", 0, nullptr, nullptr, &sound);
 
-    ma_node_attach_output_bus(&sound, 0, &delayNode, 0);
+    // ma_node_attach_output_bus(&sound, 0, &delayNode, 0);
     // ma_sound_set_start_time_in_milliseconds(&sound, 5000.0f);
     // ma_sound_start(&sound);
 
@@ -141,7 +149,67 @@ int main(int argc, char** argv)
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL2_Init();
 
+#if 0
     ImGui::StyleColorsSpectrum();
+#else
+    ImGuiStyle* style = &ImGui::GetStyle();
+
+    style->WindowPadding = ImVec2(15, 15);
+    style->WindowRounding = 5.0f;
+    style->FramePadding = ImVec2(5, 5);
+    style->FrameRounding = 4.0f;
+    style->ItemSpacing = ImVec2(12, 8);
+    style->ItemInnerSpacing = ImVec2(8, 6);
+    style->IndentSpacing = 25.0f;
+    style->ScrollbarSize = 15.0f;
+    style->ScrollbarRounding = 9.0f;
+    style->GrabMinSize = 5.0f;
+    style->GrabRounding = 3.0f;
+
+    style->Colors[ImGuiCol_Text] = ImVec4(0.40f, 0.39f, 0.38f, 1.00f);
+    style->Colors[ImGuiCol_TextDisabled] = ImVec4(0.40f, 0.39f, 0.38f, 0.77f);
+    style->Colors[ImGuiCol_WindowBg] = ImVec4(0.92f, 0.91f, 0.88f, 0.70f);
+    style->Colors[ImGuiCol_ChildBg] = ImVec4(1.00f, 0.98f, 0.95f, 0.58f);
+    style->Colors[ImGuiCol_PopupBg] = ImVec4(0.92f, 0.91f, 0.88f, 0.92f);
+    style->Colors[ImGuiCol_Border] = ImVec4(0.84f, 0.83f, 0.80f, 0.65f);
+    style->Colors[ImGuiCol_BorderShadow] = ImVec4(0.92f, 0.91f, 0.88f, 0.00f);
+    style->Colors[ImGuiCol_FrameBg] = ImVec4(1.00f, 0.98f, 0.95f, 1.00f);
+    style->Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.99f, 1.00f, 0.40f, 0.78f);
+    style->Colors[ImGuiCol_FrameBgActive] = ImVec4(0.26f, 1.00f, 0.00f, 1.00f);
+    style->Colors[ImGuiCol_TitleBg] = ImVec4(1.00f, 0.98f, 0.95f, 1.00f);
+    style->Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(1.00f, 0.98f, 0.95f, 0.75f);
+    style->Colors[ImGuiCol_TitleBgActive] = ImVec4(0.25f, 1.00f, 0.00f, 1.00f);
+    style->Colors[ImGuiCol_MenuBarBg] = ImVec4(1.00f, 0.98f, 0.95f, 0.47f);
+    style->Colors[ImGuiCol_ScrollbarBg] = ImVec4(1.00f, 0.98f, 0.95f, 1.00f);
+    style->Colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.00f, 0.00f, 0.00f, 0.21f);
+    style->Colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.90f, 0.91f, 0.00f, 0.78f);
+    style->Colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.25f, 1.00f, 0.00f, 1.00f);
+    // style->Colors[ImGuiCol_ComboBg] = ImVec4(1.00f, 0.98f, 0.95f, 1.00f);
+    style->Colors[ImGuiCol_CheckMark] = ImVec4(0.25f, 1.00f, 0.00f, 0.80f);
+    style->Colors[ImGuiCol_SliderGrab] = ImVec4(0.00f, 0.00f, 0.00f, 0.14f);
+    style->Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.25f, 1.00f, 0.00f, 1.00f);
+    style->Colors[ImGuiCol_Button] = ImVec4(0.00f, 0.00f, 0.00f, 0.14f);
+    style->Colors[ImGuiCol_ButtonHovered] = ImVec4(0.99f, 1.00f, 0.22f, 0.86f);
+    style->Colors[ImGuiCol_ButtonActive] = ImVec4(0.25f, 1.00f, 0.00f, 1.00f);
+    style->Colors[ImGuiCol_Header] = ImVec4(0.25f, 1.00f, 0.00f, 0.76f);
+    style->Colors[ImGuiCol_HeaderHovered] = ImVec4(0.25f, 1.00f, 0.00f, 0.86f);
+    style->Colors[ImGuiCol_HeaderActive] = ImVec4(0.25f, 1.00f, 0.00f, 1.00f);
+    // style->Colors[ImGuiCol_Column] = ImVec4(0.00f, 0.00f, 0.00f, 0.32f);
+    // style->Colors[ImGuiCol_ColumnHovered] = ImVec4(0.25f, 1.00f, 0.00f, 0.78f);
+    // style->Colors[ImGuiCol_ColumnActive] = ImVec4(0.25f, 1.00f, 0.00f, 1.00f);
+    style->Colors[ImGuiCol_ResizeGrip] = ImVec4(0.00f, 0.00f, 0.00f, 0.04f);
+    style->Colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.25f, 1.00f, 0.00f, 0.78f);
+    style->Colors[ImGuiCol_ResizeGripActive] = ImVec4(0.25f, 1.00f, 0.00f, 1.00f);
+    // style->Colors[ImGuiCol_CloseButton] = ImVec4(0.40f, 0.39f, 0.38f, 0.16f);
+    // style->Colors[ImGuiCol_CloseButtonHovered] = ImVec4(0.40f, 0.39f, 0.38f, 0.39f);
+    // style->Colors[ImGuiCol_CloseButtonActive] = ImVec4(0.40f, 0.39f, 0.38f, 1.00f);
+    style->Colors[ImGuiCol_PlotLines] = ImVec4(0.40f, 0.39f, 0.38f, 0.63f);
+    style->Colors[ImGuiCol_PlotLinesHovered] = ImVec4(0.25f, 1.00f, 0.00f, 1.00f);
+    style->Colors[ImGuiCol_PlotHistogram] = ImVec4(0.40f, 0.39f, 0.38f, 0.63f);
+    style->Colors[ImGuiCol_PlotHistogramHovered] = ImVec4(0.25f, 1.00f, 0.00f, 1.00f);
+    style->Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.25f, 1.00f, 0.00f, 0.43f);
+    // style->Colors[ImGuiCol_ModalWindowDarkening] = ImVec4(1.00f, 0.98f, 0.95f, 0.73f);
+#endif
 
     while (!glfwWindowShouldClose(window))
     {
@@ -154,10 +222,53 @@ int main(int argc, char** argv)
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        
+        static bool use_work_area = false;
+        static ImGuiWindowFlags flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings;
 
-        ImGui::Begin("Test1");
-        ImGui::Text("Hello");
+        // We demonstrate using the full viewport area or the work area (without menu-bars, task-bars etc.)
+        // Based on your use case you may want one or the other.
+        const ImGuiViewport* viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(use_work_area ? viewport->WorkPos : viewport->Pos);
+        ImGui::SetNextWindowSize(use_work_area ? viewport->WorkSize : viewport->Size);
+
+        ImGui::Begin("App", 0, flags);
+        
+        const char* playbackDevicePreview = pPlaybackInfos[playbackIndex].name;
+        if (ImGui::BeginCombo("Playback Device", playbackDevicePreview))
+        {
+            for (int n = 0; n < playbackCount; n++)
+            {
+                ma_uint32 oldIndex = playbackIndex;
+
+                const bool is_selected = (playbackIndex == n);
+                if (ImGui::Selectable(pPlaybackInfos[n].name, is_selected))
+                    playbackIndex = n;
+
+                // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
+                if (is_selected)
+                    ImGui::SetItemDefaultFocus();
+
+                if (oldIndex != playbackIndex)
+                {
+                    // ma_device_init()
+
+                    ma_sound_stop(&sound);
+                    ma_sound_uninit(&sound);
+                    ma_engine_uninit(&engine);
+
+                    engineConfig.pPlaybackDeviceID = &pPlaybackInfos[playbackIndex].id;
+                    result = ma_engine_init(&engineConfig, &engine);
+                    if (result != MA_SUCCESS)
+                    {
+                        return result;  // Failed to initialize the engine.
+                    }
+
+                    result = ma_sound_init_from_file(&engine, "eple.mp3", 0, nullptr, nullptr, &sound);
+                }
+            }
+            ImGui::EndCombo();
+        }
+
         if (ImGui::Button("Play")) 
         {
             static bool playing = false;
@@ -170,10 +281,7 @@ int main(int argc, char** argv)
         }
         ImGui::End();
 
-        ImGui::Begin("Test2");
-        ImGui::End();
-
-        ImGui::ShowDemoWindow();
+        // ImGui::ShowDemoWindow();
 
         ImGui::Render();
         ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
