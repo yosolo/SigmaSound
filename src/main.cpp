8b46ac13fa3f234bac3d1f73c0461d18215a5786
Yosolo <LeonardButz@outlook.de>
2024-08-12 23:22:01 +0200

    ImGui node test

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,216 @@
 #include <imgui_demo.cpp>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl2.h>
+#include <imgui_node_editor.h>
 
 #include "imgui_spectrum.h"
+
+namespace ed = ax::NodeEditor;
+
+struct Example
+{
+    // Struct to hold basic information about connection between
+    // pins. Note that connection (aka. link) has its own ID.
+    // This is useful later with dealing with selections, deletion
+    // or other operations.
+    struct LinkInfo
+    {
+        ed::LinkId Id;
+        ed::PinId  InputId;
+        ed::PinId  OutputId;
+    };
+
+    void OnStart()
+    {
+        ed::Config config;
+        config.SettingsFile = "BasicInteraction.json";
+        m_Context = ed::CreateEditor(&config);
+    }
+
+    void OnStop()
+    {
+        ed::DestroyEditor(m_Context);
+    }
+
+    void ImGuiEx_BeginColumn()
+    {
+        ImGui::BeginGroup();
+    }
+
+    void ImGuiEx_NextColumn()
+    {
+        ImGui::EndGroup();
+        ImGui::SameLine();
+        ImGui::BeginGroup();
+    }
+
+    void ImGuiEx_EndColumn()
+    {
+        ImGui::EndGroup();
+    }
+
+    void OnFrame(float deltaTime)
+    {
+        auto& io = ImGui::GetIO();
+
+        ImGui::Text("FPS: %.2f (%.2gms)", io.Framerate, io.Framerate ? 1000.0f / io.Framerate : 0.0f);
+
+        ImGui::Separator();
+
+        ed::SetCurrentEditor(m_Context);
+
+        // Start interaction with editor.
+        ed::Begin("My Editor", ImVec2(0.0, 0.0f));
+
+        int uniqueId = 1;
+
+        //
+        // 1) Commit known data to editor
+        //
+
+        // Submit Node A
+        ed::NodeId nodeA_Id = uniqueId++;
+        ed::PinId  nodeA_InputPinId = uniqueId++;
+        ed::PinId  nodeA_OutputPinId = uniqueId++;
+
+        if (m_FirstFrame)
+            ed::SetNodePosition(nodeA_Id, ImVec2(10, 10));
+        ed::BeginNode(nodeA_Id);
+        ImGui::Text("Node A");
+        ed::BeginPin(nodeA_InputPinId, ed::PinKind::Input);
+        ImGui::Text("-> In");
+        ed::EndPin();
+        ImGui::SameLine();
+        ed::BeginPin(nodeA_OutputPinId, ed::PinKind::Output);
+        ImGui::Text("Out ->");
+        ed::EndPin();
+        ed::EndNode();
+
+        // Submit Node B
+        ed::NodeId nodeB_Id = uniqueId++;
+        ed::PinId  nodeB_InputPinId1 = uniqueId++;
+        ed::PinId  nodeB_InputPinId2 = uniqueId++;
+        ed::PinId  nodeB_OutputPinId = uniqueId++;
+
+        if (m_FirstFrame)
+            ed::SetNodePosition(nodeB_Id, ImVec2(210, 60));
+        ed::BeginNode(nodeB_Id);
+        ImGui::Text("Node B");
+        ImGuiEx_BeginColumn();
+        ed::BeginPin(nodeB_InputPinId1, ed::PinKind::Input);
+        ImGui::Text("-> In1");
+        ed::EndPin();
+        ed::BeginPin(nodeB_InputPinId2, ed::PinKind::Input);
+        ImGui::Text("-> In2");
+        ed::EndPin();
+        ImGuiEx_NextColumn();
+        ed::BeginPin(nodeB_OutputPinId, ed::PinKind::Output);
+        ImGui::Text("Out ->");
+        ed::EndPin();
+        ImGuiEx_EndColumn();
+        ed::EndNode();
+
+        // Submit Links
+        for (auto& linkInfo : m_Links)
+            ed::Link(linkInfo.Id, linkInfo.InputId, linkInfo.OutputId);
+
+        //
+        // 2) Handle interactions
+        //
+
+        // Handle creation action, returns true if editor want to create new object (node or link)
+        if (ed::BeginCreate())
+        {
+            ed::PinId inputPinId, outputPinId;
+            if (ed::QueryNewLink(&inputPinId, &outputPinId))
+            {
+                // QueryNewLink returns true if editor want to create new link between pins.
+                //
+                // Link can be created only for two valid pins, it is up to you to
+                // validate if connection make sense. Editor is happy to make any.
+                //
+                // Link always goes from input to output. User may choose to drag
+                // link from output pin or input pin. This determine which pin ids
+                // are valid and which are not:
+                //   * input valid, output invalid - user started to drag new ling from input pin
+                //   * input invalid, output valid - user started to drag new ling from output pin
+                //   * input valid, output valid   - user dragged link over other pin, can be validated
+
+                if (inputPinId && outputPinId) // both are valid, let's accept link
+                {
+                    // ed::AcceptNewItem() return true when user release mouse button.
+                    if (ed::AcceptNewItem())
+                    {
+                        // Since we accepted new link, lets add one to our list of links.
+                        m_Links.push_back({ ed::LinkId(m_NextLinkId++), inputPinId, outputPinId });
+
+                        // Draw new link.
+                        ed::Link(m_Links.back().Id, m_Links.back().InputId, m_Links.back().OutputId);
+                    }
+
+                    // You may choose to reject connection between these nodes
+                    // by calling ed::RejectNewItem(). This will allow editor to give
+                    // visual feedback by changing link thickness and color.
+                }
+            }
+            if (ed::QueryNewNode(&inputPinId))
+            {
+                if (ed::AcceptNewItem())
+                {
+                    __debugbreak();
+                }
+            }
+        }
+        ed::EndCreate(); // Wraps up object creation action handling.
+
+
+        // Handle deletion action
+        if (ed::BeginDelete())
+        {
+            // There may be many links marked for deletion, let's loop over them.
+            ed::LinkId deletedLinkId;
+            while (ed::QueryDeletedLink(&deletedLinkId))
+            {
+                // If you agree that link can be deleted, accept deletion.
+                if (ed::AcceptDeletedItem())
+                {
+                    // Then remove link from your data.
+                    for (auto& link : m_Links)
+                    {
+                        if (link.Id == deletedLinkId)
+                        {
+                            m_Links.erase(&link);
+                            break;
+                        }
+                    }
+                }
+
+                // You may reject link deletion by calling:
+                // ed::RejectDeletedItem();
+            }
+        }
+        ed::EndDelete(); // Wrap up deletion action
+
+
+
+        // End of interaction with editor.
+        ed::End();
+
+        if (m_FirstFrame)
+            ed::NavigateToContent(0.0f);
+
+        ed::SetCurrentEditor(nullptr);
+
+        m_FirstFrame = false;
+
+        // ImGui::ShowMetricsWindow();
+    }
+
+    ed::EditorContext* m_Context = nullptr;    // Editor context, required to trace a editor state.
+    bool                 m_FirstFrame = true;    // Flag set for first frame only, some action need to be executed once.
+    ImVector<LinkInfo>   m_Links;                // List of live links. It is dynamic unless you want to create read-only view over nodes.
+    int                  m_NextLinkId = 100;     // Counter to help generate link ids. In real application this will probably based on pointer to user data structure.
+};
 
 namespace ImGui
 {
@@ -138,12 +346,12 @@ int main(int argc, char** argv)
     glfwInit();
     GLFWwindow* window = glfwCreateWindow(800, 600, "Sound", nullptr, nullptr);
     glfwMakeContextCurrent(window);
-
+    glfwSwapInterval(1);
 
     ImGui::CreateContext();
     
     ImGuiIO& io = ImGui::GetIO();
-    io.Fonts->AddFontFromFileTTF("OpenSans-Regular.ttf", 15.0f);
+    io.Fonts->AddFontFromFileTTF("OpenSans-Regular.ttf", 24.0f);
     io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable | ImGuiConfigFlags_DockingEnable;
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
@@ -151,7 +359,7 @@ int main(int argc, char** argv)
 
 #if 0
     ImGui::StyleColorsSpectrum();
-#else
+#elif 0
     ImGuiStyle* style = &ImGui::GetStyle();
 
     style->WindowPadding = ImVec2(15, 15);
@@ -210,6 +418,14 @@ int main(int argc, char** argv)
     style->Colors[ImGuiCol_TextSelectedBg] = ImVec4(0.25f, 1.00f, 0.00f, 0.43f);
     // style->Colors[ImGuiCol_ModalWindowDarkening] = ImVec4(1.00f, 0.98f, 0.95f, 0.73f);
 #endif
+
+    // ed::EditorContext* m_Context = nullptr;
+    // ed::Config config;
+    // config.SettingsFile = "Simple.json";
+    // m_Context = ed::CreateEditor(&config);
+
+    Example ex;
+    ex.OnStart();
 
     while (!glfwWindowShouldClose(window))
     {
@@ -279,6 +495,39 @@ int main(int argc, char** argv)
 
             playing = !playing;
         }
+
+        // ed::SetCurrentEditor(m_Context);
+        // ed::Begin("My Editor", ImVec2(0.0, 0.0f));
+        // int uniqueId = 1;
+        // // Start drawing nodes.
+        // ed::BeginNode(uniqueId++);
+        // ImGui::Text("Node A");
+        // ed::BeginPin(uniqueId++, ed::PinKind::Input);
+        // ImGui::Text("-> In");
+        // ed::EndPin();
+        // ImGui::SameLine();
+        // ed::BeginPin(uniqueId++, ed::PinKind::Output);
+        // ImGui::Text("Out ->");
+        // ed::EndPin();
+        // ed::EndNode();
+        // 
+        // ed::BeginNode(uniqueId++);
+        // ImGui::Text("Node B");
+        // ed::BeginPin(uniqueId++, ed::PinKind::Input);
+        // ImGui::Text("-> In");
+        // ed::EndPin();
+        // ImGui::SameLine();
+        // ed::BeginPin(uniqueId++, ed::PinKind::Output);
+        // ImGui::Text("Out ->");
+        // ed::EndPin();
+        // ed::EndNode();
+        // 
+        // 
+        // ed::End();
+        // ed::SetCurrentEditor(nullptr);
+
+        ex.OnFrame(1.0f);
+
         ImGui::End();
 
         // ImGui::ShowDemoWindow();
